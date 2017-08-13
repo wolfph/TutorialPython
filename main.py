6dd49c9f388ff9c555c739586f992fdc693d2547
wolfph <philipwolfram@Philips-iMac.local>
2017-08-13 18:48:40 +0200

    add function

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import math
+
 print("Hello World!")
 
-sum = 5+5
-
-print(sum)
+n = input("Enter a number ")
+def myfunction(n):
+    if type(n) == int and n % 3 == 0:
+        return math.sqrt(n)
+    else:
+        print("Fail")
