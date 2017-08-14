9901bf4397ef9711f97727a8ebe1dcfe947e425c
wolfph <philipwolfram@Philips-iMac.local>
2017-08-14 22:34:06 +0200

    add Dictionary

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import math
 
 print("Hello World!")
 
+my_dict = {
+  "fish": ["c", "a", "r", "p"],
+  "cash": -4483,
+  "luck": "good"
+}
+print (my_dict["fish"][0])
+
+
+
 n = input("Enter a number ")
 def myfunction(n):
     if type(n) == int and n % 3 == 0:
         return math.sqrt(n)
     else:
         print("Fail")
+
